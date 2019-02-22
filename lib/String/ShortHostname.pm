9d536f0e2904a05376cc4f9e38b55c51db15816c
Matthew Mallard <matt@Q-Technologies.com.au>
2019-02-23 01:59:20 +1100

    Tidied the code and added test cases

--- lib/String/ShortHostname.pm
+++ lib/String/ShortHostname.pm
@@ -6,29 +6,53 @@ package String::ShortHostname;
 
 use Moose;
 use Moose::Exporter;
+use 5.10.0;
 
-Moose::Exporter->setup_import_methods(
-      as_is     => [ 'short_hostname' ],
-  );
+=head1 SYNOPSIS
+
+This module will take a fully qualified domain name and return the first field which is normally the
+short hostname (mostly equivalent to C<hostname -s> on Linux).
+
+    use String::ShortHostname;
+    my $fqdn = 'testhost.example.com';
+    my $hostname = short_hostname( $fqdn );
+    print $hostname; 
+    # prints 'testhost'
+
+If an IPv4 address is passed to it, it will be returned verbatim. Otherwise the logic is simply to 
+return everything before the first C<.>.
+
+Alternatively, it can be used in an OO way, but without much benefit:
+
+    use String::ShortHostname;
+    my $fqdn = 'testhost.example.com';
+    my $short = String::ShortHostname->new( $fqdn );
+    my $hostname = $short->hostname;
+    print $hostname; 
+    # prints 'testhost'
+
+=cut
+
+Moose::Exporter->setup_import_methods( as_is => [ 'short_hostname' ]);
 
 has 'hostname' => (
-    is => 'rw', 
-    isa => 'Str',
-    required => 1,
-    default => '',
+    is        => 'rw',
+    isa       => 'Str',
+    required  => 1,
+    default   => '',
     predicate => 'has_hostname',
 );
 
 around BUILDARGS => sub {
-  my $orig  = shift;
-  my $class = shift;
+    my $orig  = shift;
+    my $class = shift;
 
-  if ( @_ == 1 && !ref $_[0] ) {
-      return $class->$orig( hostname => $_[0] );
-  }
-  else {
-      return $class->$orig(@_);
-  }
+    if ( @_ == 1 && !ref $_[0] ) {
+        return $class->$orig( hostname => short_hostname($_[0]) );
+    }
+    else {
+        return $class->$orig(@_);
+    }
 };
 
 around 'hostname' => sub {
@@ -54,5 +78,13 @@ sub short_hostname {
         return $hostname; # probably an IP Address
     }
 }
+
+=head1 BUGS/FEATURES
+
+Please report any bugs or feature requests in the issues section of GitHub: 
+L<https://github.com/Q-Technologies/perl-String-ShortHostname>. Ideally, submit a Pull Request.
+
+=cut
+
 
 1;
